f2d288ff77f5b773fdcfb054825c15c801d94d18
janburian <jburian@students.zcu.cz>
2023-11-03 17:21:41 +0100

    Small modifications

--- src/generate_audio.py
+++ src/generate_audio.py
@@ -16,11 +16,12 @@ audio_signal = t * np.sin(2 * np.pi * frequency * t)
 audio_signal = audio_signal / np.max(np.abs(audio_signal))
 
 # Create a delay
-num_delay_samples = int(0.01 * sample_rate)  # Number of delay samples
+num_delay_samples = int(0.02 * sample_rate)  # Number of delay samples
 delayed_audio_signal = np.roll(audio_signal, num_delay_samples)  # Shifting audio signal
 
 # Create a stereo audio signal
 stereo_audio_signal = np.column_stack((audio_signal, delayed_audio_signal))
 
 # Save the stereo audio signal as a WAV file
-wavfile.write(os.path.join("./data/", OUTPUT_filename), sample_rate, stereo_audio_signal.astype(np.float32))
+# wavfile.write(os.path.join("./data/", OUTPUT_filename), sample_rate, stereo_audio_signal.astype(np.float32))
+wavfile.write(os.path.join(OUTPUT_filename), sample_rate, stereo_audio_signal.astype(np.float32))
